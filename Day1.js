1f4abddde5ae280b484a6abe20ddeb6b5227f894
Julia Kester <kester@oswego.edu>
2024-12-01 00:43:14 -0500

    Day 1 complete

--- Day1.js
+++ Day1.js
@@ -5,7 +5,31 @@ fs.readFile('Day1.txt', 'utf8', function (err, data) {
   }
   let lines = data.split('\n');
 
+  const leftList = Array();
+  const rightList = Array();
+  const rightListSet = new Map();
+
   lines.forEach(line => {
-    
+    const [left, right] = line.split("   ").map(Number);
+    leftList.push(left);
+    rightList.push(right);
   });
+
+  leftList.sort((a, b) => {
+    return a - b});
+  rightList.sort((a, b) => {
+    return a-b});
+  rightList.map((val) => rightListSet[val] =  rightListSet[val] ? + rightListSet[val] + 1 : 1);
+
+  let dist = 0;
+  let similarityScore = 0;
+  leftList.map((val, i) => {
+   dist += Math.abs(val - rightList[i]);
+
+   similarityScore += val * (rightListSet[val] || 0);
+  });
+
+  console.log("P1:", {dist})
+  console.log("P2:", {similarityScore})
+
 });
