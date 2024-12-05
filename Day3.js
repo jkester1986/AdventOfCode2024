0dfa228cb9dfbb125d979ab9d3aa7c563f4db134
Julia Kester <jkester@acvauctions.com>
2024-12-05 16:03:32 -0600

    Day 3 complete

---
+++ Day3.js
@@ -0,0 +1,24 @@
+fs = require('fs');
+fs.readFile('Day3.txt', 'utf8', function (err, data) {
+  if (err) {
+    return console.log(err);
+  }
+  let lines = data.split('\n');
+  console.log(lines.length);
+
+  let sum = 0;
+  let skipMul = false;
+
+  lines.forEach(line => {
+    let matches = line.match(/(mul\((\d+),(\d+)\)|do\(\)|don't\(\))/g);
+    matches.forEach(match => {
+      if(match.includes("don't")) return skipMul = true;
+      else if (match.includes("do")) return skipMul = false;
+      if (skipMul) return;
+      let numbers = match.match(/\d+/g).map(Number);
+      sum += numbers[0] * numbers[1]
+    })
+  })
+
+  console.log("P2:", sum)
+});
