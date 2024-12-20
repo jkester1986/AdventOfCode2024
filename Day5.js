1a6e74199c6f5337c11c91d349dedc107f5d94bb
Julia Kester <kester@oswego.edu>
2024-12-20 17:19:29 -0500

    Day 5 P1 complete

--- Day5.js
+++ Day5.js
@@ -3,6 +3,23 @@ fs.readFile('Day5.txt', 'utf8', function (err, data) {
   if (err) {
     return console.log(err);
   }
-  let lines = data.split('\n');
+  let [pageOrders, updateLines] = data.split('\n\n');
+  const pageOrderSets = pageOrders.split('\n').map(line => line.split("|").map(Number));
+  updates = updateLines.split('\n').map(line => line.split(",").map(Number),);
 
+  let middleSum = 0;
+  updates.forEach(update => {
+    let middle = update[Math.floor(update.length / 2)];
+    let rulesMet = pageOrderSets.every(pageOrderSet => {
+      if (update.includes(pageOrderSet[0]) && update.includes(pageOrderSet[1]) && update.indexOf(pageOrderSet[0]) > update.indexOf(pageOrderSet[1])) {
+        return false;
+      }
+      return true;
+    });
+    if (rulesMet) {
+      middleSum += middle;
+    }
+  });
+
+  console.log("P1:", middleSum);
 });
