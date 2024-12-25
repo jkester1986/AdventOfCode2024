26457c34554aab9a24beddeb3a2ee954d7e327ab
Julia Kester <kester@oswego.edu>
2024-12-25 03:22:06 -0500

    small tweaks

--- Day25.js
+++ Day25.js
@@ -4,8 +4,7 @@ fs.readFile('Day25.txt', 'utf8', function (err, data) {
     return console.log(err);
   }
 
-  let locks = []
-  let keys = []
+  let locks = [], keys = []
   let uniquePairs = 0;
 
   let = data.split('\n\n').forEach(block => {
@@ -21,6 +20,8 @@ fs.readFile('Day25.txt', 'utf8', function (err, data) {
       let counter = 0;
       let overlap = false;
       // see if there's overlap of "#"
+      // Note that THE HEIGHT IS A LIE - it's a totally unecessary pieces of information in the directions for comparison purposes.
+      // We don't care about the heights at all, just that there's no overlap.
       while (counter < length && !overlap) {
         if (lock.charAt(counter) === "#" && key.charAt(counter) === "#") {
           overlap = true;
